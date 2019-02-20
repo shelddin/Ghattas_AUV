fa3aee935bb504588da7e23292fe7fd4c56e3b32
ahmad-aljabali <36141448+ahmad-aljabali@users.noreply.github.com>
2019-02-20 18:53:18 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# prepare system before install
 cd $HOME
+sudo apt update
+sudo apt upgrade
+sudo apt install git
 
+# ROS
 # add ROS ppa
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
@@ -14,15 +19,39 @@ sudo apt-get install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-# install additional ROS packages 
+# install additional ROSserial 
 sudo apt-get install ros-kientic-rosserial-arduino
 sudo apt-get install ros-kinetic-rosserial
 
+# creat, install needed deps and build workspace
+# clone ghattas repo to work-space
+cd $HOME
+mkdir ~/ghattas/src/
+cd ~/ghattas/
+git clone https://github.com/ShehabAldeen/Ghattas_AUV/ ~/ghattas/src/
+cd ~/ghattas/src/
+# clone flexbe behavior engine to work-space 
+git clone https://github.com/team-vigir/flexbe_behavior_engine.git
+git clone https://github.com/FlexBE/flexbe_app.git
+# install MavLink, MavROS & MavProxy
+sudo apt-get install ros-kinetic-mavros ros-kinetic-mavros-extras
+sudo pip install --upgrade pymavlink MAVProxy
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+chmod +x ./install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
+rm ./install_geographiclib_datasets.sh
+# build work=space
+source /opt/ros/kinetic/setup.bash
+cd ~/ghattas/
+catkin_make
+source devel/setup.bash
+
+# OPENCV
 # Install opencv required deps
-sudo apt install htop
 sudo apt install python-pip
 sudo pip2 install --upgrade pip
 sudo pip2 install imutils
@@ -32,7 +61,6 @@ sudo apt install gcc g++
 sudo apt install python-gtk2-dev
 sudo apt install libffms2-4
 sudo apt install gstreamer1.0-plugins-base
-sudo apt install git
 
 # clone and build latest stable opencv version
 cd $HOME
@@ -49,23 +77,8 @@ sudo make install
 #read -p 'press enter to clear temp folders' $x
 #yes | rm -r opencv-source
 
-# creat, install needed deps and build workspace
-cd $HOME
-mkdir ~/ghattas/src/
-cd ~/ghattas/
-git clone https://github.com/ShehabAldeen/Ghattas_AUV/ ~/ghattas/src/
-cd ~/ghattas/src/
-git clone https://github.com/team-vigir/flexbe_behavior_engine.git
-git clone https://github.com/FlexBE/flexbe_app.git
-sudo apt-get install ros-kinetic-mavros ros-kinetic-mavros-extras
-sudo pip install --upgrade pymavlink MAVProxy
-wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
-chmod +x ./install_geographiclib_datasets.sh
-sudo ./install_geographiclib_datasets.sh
-rm ./install_geographiclib_datasets.sh
-
-#source /opt/ros/kinetic/setup.bash
-cd ~/ghattas/
-catkin_make
-source devel/setup.bash
-
+# install optional user convenienc packages
+sudo add-apt-repository ppa:webupd8team/terminix
+sudo apt update
+sudo apt install tilix
+sudo apt install htop
