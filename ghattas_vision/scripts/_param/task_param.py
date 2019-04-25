371d65cc647b96513276742d5795b494b8075b8c
ahmad-aljabali <36141448+ahmad-aljabali@users.noreply.github.com>
2019-04-25 20:02:51 +0300

    Update task_param.py

--- ghattas_vision/scripts/_param/task_param.py
+++ ghattas_vision/scripts/_param/task_param.py
@@ -1,13 +1,3 @@
-import numpy as np
-
-world_pts={}
-world_pts['gate']=np.array([],dtype='double')
-world_pts['path']=[]
-world_pts['bouy']=[]
-world_pts['torpedo']=[]
-world_pts['dropper']=[]
-
-
 
 colors={}
 colors['gate']=['red','black','orange']
