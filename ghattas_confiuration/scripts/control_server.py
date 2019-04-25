6c8ffc895261c33bd0da45bb78710e4f6c0befeb
ShehabAldeen <shehab.pro@gmail.com>
2019-04-25 23:24:39 +0300

    edited reconfigure gui for the control package

--- ghattas_confiuration/scripts/control_server.py
+++ ghattas_confiuration/scripts/control_server.py
@@ -6,8 +6,6 @@ from dynamic_reconfigure.server import Server
 from ghattas_confiuration.cfg import control_reconfigureConfig
 
 def callback(config, level):
-#    rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},\
-#          {str_param}, {bool_param}, {size}""".format(**config))
     rospy.loginfo(config)
     return config
 
